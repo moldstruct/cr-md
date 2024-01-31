1d65290fd7b4a0a7761aab3bf09476654dd87f72
idawod <ibrahim.dawod@physics.uu.se>
2024-01-31 11:13:03 +0100

    extra notes

--- Example/Lysozyme/run_photon_matter_simulation.sh
+++ Example/Lysozyme/run_photon_matter_simulation.sh
@@ -1,10 +1,10 @@
-gmx='/home/ibrahim/programs/test/bin' # path to where GROMACS is installed
+gmx='/path/to/install/location/bin' # path to where GROMACS is installed
 
 filename="conf.gro"
 idx=1
 
-
-#exit
 $gmx/grompp -f full_sim.mdp -po mdout_$idx -c $filename -n index.ndx -p ./topology/topol.top -o output_$idx.tpr -maxwarn 3  # create .tpr file
+
 $gmx/mdrun -s output_$idx.tpr -v -deffnm output_$idx # run simulation, use -nt X, where X is number of cores you want to run with specific number of cores
+
 rm MPI_slice_n*
